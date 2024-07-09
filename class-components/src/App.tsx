9e89268a81364b07e6be4dc833ef2da21d3079dc
Joelle <joelle.marianek@live.de>
2024-07-09 17:05:51 +0200

    feat: fetch series data

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,14 +1,46 @@
 import React from 'react'
-import './App.css'
+import './index.css'
+import { AppState } from './types/seriesInterface';
 
-export default class App extends React.Component {
+export default class App extends React.Component<object, AppState> {
+    constructor(props: object) {
+        super(props)
+        this.state = {
+            series: []
+        };
+    //     this.handleClick = this.handleClick.bind(this)
+    }
+
+    componentDidMount(): void {
+        fetch("https://stapi.co/api/v1/rest/series/search?pageNumber=0")
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({ series: data?.series})
+        })
+        .catch(error => console.error("Error fetching data", error))
+    }
+    // handleClick(e) {
+
+    // }
     render() {
         return (
             <div className="wrapper">
-                <div>
-
+                <div className="searchbar">
+                    <input type="text" defaultValue="Search"/>
+                    <button>Search</button>
                 </div>
-                <div>
+                <div className="results-list">
+                    {this.state.series.map(serie => (
+                        <div key={serie.uid} className="list-item">
+                            <h3>{serie.title} ({serie.abbreviation})</h3>
+                            <p>Production Years: {serie.productionStartYear} - {serie.productionEndYear || 'Ongoing'}</p>
+                            <p>Original Run: {serie.originalRunStartDate} to {serie.originalRunEndDate || 'Ongoing'}</p>
+                            <p>Seasons: {serie.seasonsCount}</p>
+                            <p>Episodes: {serie.episodesCount}</p>
+                            <p>Production Company: {serie.productionCompany.name}</p>
+                            <p>Original Broadcaster: {serie.originalBroadcaster.name}</p>
+                        </div>
+                    ))}
 
                 </div>
 
