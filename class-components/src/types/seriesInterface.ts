89037914b2f2a938f5a86b155b2efb05b0039315
Joelle <joelle.marianek@live.de>
2024-07-09 17:06:43 +0200

    feat: implement types

---
+++ class-components/src/types/seriesInterface.ts
@@ -0,0 +1,22 @@
+ export interface Series {
+
+    uid: string;
+    title: string;
+    abbreviation: string;
+    productionStartYear: number;
+    productionEndYear: number | null;
+    originalRunStartDate:string | null;
+    originalRunEndDate: string | null;
+    seasonsCount: number | null;
+    episodesCount: number | null;
+    productionCompany: {
+        name: string
+    };
+    originalBroadcaster: {
+         name: string
+    }
+}
+
+export interface AppState {
+    series: Series[];
+}
