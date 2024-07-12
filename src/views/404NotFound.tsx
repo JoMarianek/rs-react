bc532463878a6673cb1f3fd1ccc4bf06d23abf74
Joelle <joelle.marianek@live.de>
2024-07-12 23:14:57 +0200

    feat: 404NotFound Page

---
+++ src/views/404NotFound.tsx
@@ -0,0 +1,13 @@
+
+const PageNotFound = () => {
+    return (
+        <>
+        <h1>Oops!</h1>
+        <h2> 404 - Page not found</h2>
+        <p>The page you are looking for might have been removed, had its name changed
+            or is temporary unavailable</p>
+        </>
+    )
+}
+
+export default PageNotFound;
