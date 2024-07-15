7e6d584af6518389a078f5d0ce6640a604045515
Joelle <joelle.marianek@live.de>
2024-07-15 16:24:20 +0200

    refactor: adjust styling of 404 page

--- src/views/404NotFound.tsx
+++ src/views/404NotFound.tsx
@@ -1,13 +1,17 @@
+
 import { Link } from "react-router-dom";
+
+import styles from "./404NotFound.module.css"
 const PageNotFound = () => {
     return (
-        <>
-        <h1>Oops!</h1>
-        <h2> 404 - Page not found</h2>
-        <p>The page you are looking for might have been removed, had its name changed
-            or is temporary unavailable</p>
+        <div className={styles.notFound}>
+            <h1>Oops!</h1>
+            <h2> 404 - Page not found</h2>
+            <p>The page you are looking for might have been removed, had its name changed
+                or is temporary unavailable
+            </p>
             <Link to="/">Go to Homepage</Link>
-        </>
+        </div>
     )
 }
 
